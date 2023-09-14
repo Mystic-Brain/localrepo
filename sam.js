12ac6c61218c780b58075c93d2d8d5cda76c8ccd
Mystic-Brain <sawalesanket@gmail.com>
2023-09-14 13:11:54 +0530

    first changeeee

---
+++ sam.js
@@ -0,0 +1,14 @@
+const laptop = {
+    cool:'sanket',
+    time:'pass'
+}
+console.log(laptop);
+console.log(laptop.cool);
+
+let language = 'javascript';
+console.log(language)
+
+
+let age =25;
+age = 'twenty five'
+console.log(age)
